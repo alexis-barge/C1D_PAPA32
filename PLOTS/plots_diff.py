aef43255965b6b619c5dbbe103bfd3709271d731
Alexis Barge <alexis.barge@proton.me>
2025-01-24 14:00:15 +0100

    adapt script to test case

--- PLOTS/plots_diff.py
+++ PLOTS/plots_diff.py
@@ -65,16 +65,16 @@ def main(filepath_ref, ref_name, filepath, var_name, fig_name, infos, freq):
     time_counter = time_counter[:var_ref.shape[1]]
 
     # plot
-    plotpath = 'C1D_PAPA_DNN_' + freq + '_' + fig_name +'_error.png'
+    plotpath = fig_name + '_error_C1D_PAPA32_' + freq + '.png'
     make_plot(diff_var,time_counter,dpt,infos,plotpath)
 
 
 
 if __name__=="__main__":
     # temperature profiles error
-    infos = [ 'T_DNN - T_OBS (ºC)' , cmocean.cm.balance , colors.Normalize(vmin=-1.5, vmax=1.5), lambda x: x ]
-    main( filepath_ref='OSP32_obs_T.nc' , ref_name='T_20', filepath='C1D_PAPA_DNN_1d_20100615_20110614_grid_T.nc' , var_name='votemper' , fig_name='T' , infos=infos , freq='1d' )
+    infos = [ 'T - T_OBS (ºC)' , cmocean.cm.balance , colors.Normalize(vmin=-1.5, vmax=1.5), lambda x: x ]
+    main( filepath_ref='OSP32_obs_T.nc' , ref_name='T_20', filepath='C1D_PAPA32_1d_20100615_20110614_grid_T.nc' , var_name='votemper' , fig_name='T' , infos=infos , freq='1d' )
 
     # salinity profiles error
-    infos = [ 'S_DNN - S_OBS (psu)' , cmocean.cm.balance , colors.Normalize(vmin=-0.2, vmax=0.2), lambda x: x ]
-    main( filepath_ref='OSP32_obs_S.nc' , ref_name='S_41', filepath='C1D_PAPA_DNN_1d_20100615_20110614_grid_T.nc' , var_name='vosaline' , fig_name='S' , infos=infos , freq='1d' )
+    infos = [ 'S - S_OBS (psu)' , cmocean.cm.balance , colors.Normalize(vmin=-0.2, vmax=0.2), lambda x: x ]
+    main( filepath_ref='OSP32_obs_S.nc' , ref_name='S_41', filepath='C1D_PAPA32_1d_20100615_20110614_grid_T.nc' , var_name='vosaline' , fig_name='S' , infos=infos , freq='1d' )
